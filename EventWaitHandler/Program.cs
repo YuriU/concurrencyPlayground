76a5a47009f549addbd54b03536b8cf864a1dc2f
YuriU <jerryhire8@gmail.com>
2020-03-14 19:05:49 +0200

    Initial version

---
+++ EventWaitHandler/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EventWaitHandler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
