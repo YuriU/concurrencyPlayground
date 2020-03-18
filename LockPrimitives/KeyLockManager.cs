98c4fd9fee69c39691f5b651f54e84672c913559
YuriU <jerryhire8@gmail.com>
2020-03-18 21:41:19 +0200

    Created pinnable object

--- LockPrimitives/KeyLockManager.cs
+++ LockPrimitives/KeyLockManager.cs
@@ -4,13 +4,11 @@ using System.Threading;
 
 namespace LockPrimitives
 {
-    public class KeyLockItem<T> : IDisposable
+    public class KeyLockItem<T> : PinableObject, IDisposable
     {
         public readonly T Key;
 
         public readonly KeyLockManager<T> KeyLockManager;
-
-        private int _refCounter;
 
         private object LockObject = new object();
 
@@ -20,7 +18,6 @@ namespace LockPrimitives
         {
             Key = key;
             KeyLockManager = keyLockManager;
-            _refCounter = 1;
         }
 
         public void Lock()
@@ -33,42 +30,6 @@ namespace LockPrimitives
         {
             _locked = false;
             Monitor.Exit(LockObject);
-        }
-
-        // Trying to keep RefCounter to be positive value
-        // Once it is equal to 0, the item can be disposed, so we lost the race
-        public bool Pin()
-        {
-            // Spinning while one of the following cases happen
-            while (true)
-            {
-                int refCounter = _refCounter;
-
-                // 1. The item is released by it's previous owner, so it can be deleted from the repository
-                if (refCounter == 0)
-                {
-                    return false;
-                }
-
-                // 2. We added our reference to the counter, so while we release it, noone can delete it
-                if (Interlocked.CompareExchange(ref _refCounter, refCounter + 1, refCounter) == refCounter)
-                {
-                    return true;
-                }
-            }
-        }
-
-        public bool Unpin()
-        {
-            var value = Interlocked.Decrement(ref _refCounter);
-            if (value == 0)
-            {
-                return true;
-            }
-            else
-            {
-                return false;
-            }
         }
 
         public void Dispose()
---
+++ LockPrimitives/PinableObjecsPool.cs
@@ -0,0 +1,55 @@
+using System.Threading;
+
+namespace LockPrimitives
+{
+    public class PinableObject
+    {
+        private int _refCounter;
+        
+        protected PinableObject()
+        {
+            _refCounter = 1;
+        }
+        
+        // Trying to keep RefCounter to be positive value
+        // Once it is equal to 0, the item can be disposed, so we lost the race
+        public bool Pin()
+        {
+            // Spinning while one of the following cases happen
+            while (true)
+            {
+                int refCounter = _refCounter;
+
+                // 1. The item is released by it's previous owner, so it can be deleted from the repository
+                if (refCounter == 0)
+                {
+                    return false;
+                }
+
+                // 2. We added our reference to the counter, so while we release it, noone can delete it
+                if (Interlocked.CompareExchange(ref _refCounter, refCounter + 1, refCounter) == refCounter)
+                {
+                    return true;
+                }
+            }
+        }
+
+        public bool Unpin()
+        {
+            var value = Interlocked.Decrement(ref _refCounter);
+            if (value == 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+    
+    public class PinableObjecsPool
+    {
+        
+    }
+}
