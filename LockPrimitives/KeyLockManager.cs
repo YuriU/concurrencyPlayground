f0a476cbd3f0ba9cbcba9e1af853a3a557989157
YuriU <jerryhire8@gmail.com>
2020-03-18 22:16:10 +0200

    Extracting base object pool

--- LockPrimitives/KeyLockManager.cs
+++ LockPrimitives/KeyLockManager.cs
@@ -4,21 +4,16 @@ using System.Threading;
 
 namespace LockPrimitives
 {
-    public class KeyLockItem<T> : PinableObject, IDisposable
+    public class KeyLockItem<T> 
+        : PinableObject<T, KeyLockItem<T>>, IDisposable
     {
-        public readonly T Key;
-
-        public readonly KeyLockManager<T> KeyLockManager;
-
         private object LockObject = new object();
 
         private bool _locked = false;
 
         public KeyLockItem(T key, KeyLockManager<T> keyLockManager)
-        {
-            Key = key;
-            KeyLockManager = keyLockManager;
-        }
+            : base(key, keyLockManager)
+        { }
 
         public void Lock()
         {
@@ -37,57 +32,15 @@ namespace LockPrimitives
             if(_locked)
                 Unlock();
 
-            KeyLockManager.ReturnLockItem(this);
+            ReturnToPool();
         }
     }
 
-    public class KeyLockManager<T>
+    public class KeyLockManager<T> : PinableObjecsPool<T, KeyLockItem<T>>
     {
-        private ConcurrentDictionary<T, KeyLockItem<T>> _keyToLockItems = new ConcurrentDictionary<T, KeyLockItem<T>>();
-        
-        public KeyLockItem<T> GetLockItem(T key)
+        protected override KeyLockItem<T> NewObject(T key)
         {
-            while (true)
-            {
-                var newItem = new KeyLockItem<T>(key, this);
-
-                var item = _keyToLockItems.GetOrAdd(key, newItem);
-
-                // Item was just added. The items are created with refCount = 1, so we don't need to pin it
-                if (newItem == item)
-                {
-                    return item;
-                }
-                // Item was already in dictionary. Trying to pin it
-                else
-                {
-                    // Pinned successfully other thread will not delete it
-                    if (item.Pin())
-                    {
-                        return item;
-                    }
-
-                    // The item was just successfully unpinned. and is about to be deleted
-                    // We got to the moment it is still in dictionary, but already unpinned.
-                    // Going to the next round
-                    continue;
-                }
-            }
-        }
-
-        public void ReturnLockItem(KeyLockItem<T> item)
-        {
-            if (item.KeyLockManager != this)
-            {
-                throw new ArgumentException("The item does not belong to current KeyLockManager");
-            }
-
-            // Try to unpin item. Set RefCounter to 0
-            if (item.Unpin())
-            {
-                // Here we are free do delete it. Even if some of the threads refer to the item, they would skip it and wait until we delete it
-                _keyToLockItems.TryRemove(item.Key, out var item2);
-            }
+            return new KeyLockItem<T>(key, this);
         }
     }
 }
--- LockPrimitives/PinableObjecsPool.cs
+++ LockPrimitives/PinableObjecsPool.cs
@@ -1,55 +1,57 @@
-using System.Threading;
+using System;
+using System.Collections.Concurrent;
 
 namespace LockPrimitives
 {
-    public class PinableObject
+    public abstract class PinableObjecsPool<TKey, TObject>
+        where TObject : PinableObject<TKey, TObject>
     {
-        private int _refCounter;
-        
-        protected PinableObject()
+        private ConcurrentDictionary<TKey, TObject> _keyToLockItems = new ConcurrentDictionary<TKey, TObject>();
+
+        protected abstract TObject NewObject(TKey key);
+        public TObject GetLockItem(TKey key)
         {
-            _refCounter = 1;
-        }
-        
-        // Trying to keep RefCounter to be positive value
-        // Once it is equal to 0, the item can be disposed, so we lost the race
-        public bool Pin()
-        {
-            // Spinning while one of the following cases happen
             while (true)
             {
-                int refCounter = _refCounter;
+                var newItem = NewObject(key);
 
-                // 1. The item is released by it's previous owner, so it can be deleted from the repository
-                if (refCounter == 0)
+                var item = _keyToLockItems.GetOrAdd(key, newItem);
+
+                // Item was just added. The items are created with refCount = 1, so we don't need to pin it
+                if (newItem == item)
                 {
-                    return false;
+                    return item;
                 }
-
-                // 2. We added our reference to the counter, so while we release it, noone can delete it
-                if (Interlocked.CompareExchange(ref _refCounter, refCounter + 1, refCounter) == refCounter)
+                // Item was already in dictionary. Trying to pin it
+                else
                 {
-                    return true;
+                    // Pinned successfully other thread will not delete it
+                    if (item.Pin())
+                    {
+                        return item;
+                    }
+
+                    // The item was just successfully unpinned. and is about to be deleted
+                    // We got to the moment it is still in dictionary, but already unpinned.
+                    // Going to the next round
+                    continue;
                 }
             }
         }
-
-        public bool Unpin()
-        {
-            var value = Interlocked.Decrement(ref _refCounter);
-            if (value == 0)
-            {
-                return true;
-            }
-            else
-            {
-                return false;
-            }
-        }
-    }
-    
-    public class PinableObjecsPool
-    {
         
+        public void ReturnLockItem(PinableObject<TKey, TObject> item)
+        {
+            if (item.Pool != this)
+            {
+                throw new ArgumentException("The item does not belong to current KeyLockManager");
+            }
+
+            // Try to unpin item. Set RefCounter to 0
+            if (item.Unpin())
+            {
+                // Here we are free do delete it. Even if some of the threads refer to the item, they would skip it and wait until we delete it
+                _keyToLockItems.TryRemove(item.Key, out var item2);
+            }
+        }
     }
 }
