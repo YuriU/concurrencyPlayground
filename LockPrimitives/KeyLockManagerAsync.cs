c4fa7342139abf6f3fe89b0ab26011bc2d4c7a60
YuriU <jerryhire8@gmail.com>
2020-03-18 22:24:05 +0200

    Created draft of AsyncKeyLockManager

--- LockPrimitives/KeyLockManagerAsync.cs
+++ LockPrimitives/KeyLockManagerAsync.cs
@@ -1,7 +1,25 @@
+using System;
+
 namespace LockPrimitives
 {
-    public class KeyLockManagerAsync
+    public class KeyLockAsyncItem<T>
+        : PinableObject<T, KeyLockAsyncItem<T>>, IDisposable
     {
-        
+        public void Dispose()
+        {
+        }
+
+        public KeyLockAsyncItem(T key, PinableObjecsPool<T, KeyLockAsyncItem<T>> pool) 
+            : base(key, pool)
+        {
+        }
+    }
+    
+    public class KeyLockManagerAsync<T> : PinableObjecsPool<T, KeyLockAsyncItem<T>>
+    {
+        protected override KeyLockAsyncItem<T> NewObject(T key)
+        {
+            return new KeyLockAsyncItem<T>(key, this);
+        }
     }
 }
