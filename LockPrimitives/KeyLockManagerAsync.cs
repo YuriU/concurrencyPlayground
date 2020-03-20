aa7b1ae7b73b7f1863ccffe51fd9faa20f8881a1
YuriU <jerryhire8@gmail.com>
2020-03-20 12:26:06 +0200

    Attempt to make asynk KeyLockManager

--- LockPrimitives/KeyLockManagerAsync.cs
+++ LockPrimitives/KeyLockManagerAsync.cs
@@ -1,12 +1,34 @@
 using System;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace LockPrimitives
 {
     public class KeyLockAsyncItem<T>
         : ConcurrentObjectPoolItem<T, KeyLockAsyncItem<T>>, IDisposable
     {
+        SemaphoreSlim _semaphore = new SemaphoreSlim(1, 1);
+        
+        private bool _locked = false;
+
+        public async Task WaitAsync()
+        {
+            await _semaphore.WaitAsync();
+            _locked = true;
+        }
+
+        public void Release()
+        {
+            _locked = false;
+            _semaphore.Release(1);
+        }
+        
         public void Dispose()
         {
+            if(_locked)
+                Release();
+            
+            ReturnToPool();
         }
 
         public KeyLockAsyncItem(T key, ConcurrentObjecsPool<T, KeyLockAsyncItem<T>> pool) 
